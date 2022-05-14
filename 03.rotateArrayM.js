9557c76c8991c0ad1d76115ebae2e5889f2706b1
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-05-14 21:34:23 +0200

    Add files via upload

---
+++ 03.rotateArrayM.js
@@ -0,0 +1,14 @@
+function solve(arr){
+let rotations = Number(arr.pop());
+
+for(let i = 0;i < rotations; i ++){
+    let last = arr.pop();
+    arr.unshift(last);
+}
+
+
+console.log(arr.join(` `));
+
+}
+solve(['1', '2', '3', '4', '2']);
+solve(['Banana', 'Orange', 'Coconut', 'Apple', '15']);
